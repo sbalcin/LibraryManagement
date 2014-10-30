b08dcb2f494dadc52fa0b315790965c1ebaffba3
unknown <sinan.balcin@SINANBAL-NB.hititdc.com>
2014-10-30 15:27:39 +0200

    db config and ssl for captcha

--- src/main/java/com/snn/library/utility/DBConfig.java
+++ src/main/java/com/snn/library/utility/DBConfig.java
@@ -1,26 +1,29 @@
 package com.snn.library.utility;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
- 
+import com.mongodb.MongoClientURI;
+
 @Configuration
 public class DBConfig extends AbstractMongoConfiguration {
- 
+
 	public static final String SERVER_ADDRESS = "mongodb://library:library@dogen.mongohq.com:10081/library";
-	public static final String DATABASE_NAME= "library";
-	public static final String COLLECTION_NAME= "book";
+	public static final String DATABASE_NAME = "library";
+	public static final String COLLECTION_NAME = "book";
+
 	@Override
-	
 	public String getDatabaseName() {
 		return DATABASE_NAME;
 	}
- 
+
 	@Override
 	@Bean
 	public Mongo mongo() throws Exception {
-		return new MongoClient(SERVER_ADDRESS);
+		MongoClientURI clientURI = new MongoClientURI(SERVER_ADDRESS);
+		return new MongoClient(clientURI);
 	}
 }
