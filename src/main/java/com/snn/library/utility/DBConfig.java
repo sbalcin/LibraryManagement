fc6e6d197f3da44928bc056a9ea7de3fef3c4cf7
unknown <sinan.balcin@SINANBAL-NB.hititdc.com>
2014-10-30 15:02:04 +0200

    mongo db url updated

--- src/main/java/com/snn/library/utility/DBConfig.java
+++ src/main/java/com/snn/library/utility/DBConfig.java
@@ -9,7 +9,7 @@ import com.mongodb.MongoClient;
 @Configuration
 public class DBConfig extends AbstractMongoConfiguration {
  
-	public static final String SERVER_ADDRESS = "localhost";
+	public static final String SERVER_ADDRESS = "mongodb://library:library@dogen.mongohq.com:10081/library";
 	public static final String DATABASE_NAME= "library";
 	public static final String COLLECTION_NAME= "book";
 	@Override
